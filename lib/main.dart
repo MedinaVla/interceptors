765496602baa02ff62cdaba1418ee591b8ccdb35
MedinaVla <medinavladimir14@gmail.com>
2022-03-04 10:08:07 -0500

    comment widget_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:interceptors/boostrap.dart';
 import 'package:interceptors/services/locator.dart';
+
+import 'app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
