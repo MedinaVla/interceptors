66ce0e74ba05003b2427bc59e8b75d143e8f9a89
MedinaVla <medinavladimir14@gmail.com>
2022-03-04 10:19:05 -0500

    add AuthEvent and his children

--- lib/bloc/auth_event.dart
+++ lib/bloc/auth_event.dart
@@ -1,4 +1,41 @@
 part of 'auth_bloc.dart';
 
 @immutable
-abstract class AuthEvent {}
+abstract class AuthEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class LogInRequested extends AuthEvent {
+  LogInRequested(
+    this.email,
+    this.password,
+  );
+  final String email;
+  final String password;
+  @override
+  List<Object> get props => [
+        email,
+        password,
+      ];
+}
+
+class LogInEvent extends AuthEvent {}
+
+class RegisterEvent extends AuthEvent {}
+
+class ResgisterRequested extends AuthEvent {
+  ResgisterRequested(this.email, this.password, this.userName);
+
+  final String email;
+  final String password;
+  final String userName;
+  @override
+  List<Object> get props => [
+        email,
+        password,
+        userName,
+      ];
+}
+
+class LogOutRequested extends AuthEvent {}
